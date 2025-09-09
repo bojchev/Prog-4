686592e7b49eb4a1454c5145a25dd2ffef28cf99
bojchev <nikolabojcev2008@gmail.com>
2025-09-09 20:37:23 +0200

    adjlist

--- graph-list.cpp
+++ graph-list.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
 #include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    vector<vector<int>> l(n);
+    while(true)
+    {
+        int u,v;
+        cin>>u>>v;
+        if(u==-1 && v==-1)
+        {
+            break;
+        }
+        l[u].push_back(v);
+        l[v].push_back(u);
+
+
+    }
+
+    for (int i=0;i<n;i++) {
+        cout <<i<<": ";
+        for (int j=0;j<l[i].size(); j++) {
+            cout<<l[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+
+    return 0;
+
+
+}
